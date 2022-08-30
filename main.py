b0106e23ffdfd1fcc095d2b81c6aedd602e9cfb8
MatheuzFelix <matheuzfelixamorim@gmail.com>
2022-08-30 19:47:24 -0300

    criando o main

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+import PySimpleGUI as sg
+
 from recognizer import listen_mic, write_txt, text_speech, read_file
+
+
+sg.theme('Reddit')
+layout = [
+    [sg.Text(('Selecione a opção desejada'), size=(22, 1), justification='center')],
+    [sg.Checkbox('Traduzir por voz', key='audio'),
+        sg.Checkbox('Traduzir por texto', key='text'),
+    ],
+    [sg.Button('enviar', size=(8, 0))]
+]
+window = sg.Window('Recognizer', layout, finalize=True)
+#window['id'].bind('<Return>', '_Enter')
+
+while True:
+    event, value = window.read()
+    if event == 'enviar' or event == '_Enter':
+        print(event)
+        if value['audio'] == True:
+            listen_mic()
+            write_txt()
+        elif value['text'] == True:
+            read_file()
+    break
 
 
